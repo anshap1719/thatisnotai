1daa419aaa6b92252e11fb1b18947144f81048d6
Anshul Sanghi <anshul@anshulsanghi.tech>
2025-06-19 19:09:22 +0530

    Fix About Page Link & Content

--- packages/blog-starter-kit/themes/hashnode/pages/about.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/about.tsx
@@ -1,7 +1,7 @@
 import CustomImage from '../components/custom-image';
 import PublicationSubscribeStandOut from '../components/publication-subscribe-standout';
-import { resizeImage } from '../utils/image';
-import { AppProvider } from '../components/contexts/appContext';
+import {resizeImage} from '../utils/image';
+import {AppProvider} from '../components/contexts/appContext';
 
 import BlogPostPreview from '../components/magazine-blog-post-preview';
 import {
@@ -10,11 +10,11 @@ import {
     PostThumbnailFragment,
     PublicationFragment,
 } from '../generated/graphql';
-import { createHeaders, createSSRExchange, getUrqlClientConfig } from '../lib/api/client';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { log as _log } from 'next-axiom';
-import { initUrqlClient } from 'next-urql';
-import { Header } from '../components/header';
+import {createHeaders, createSSRExchange, getUrqlClientConfig} from '../lib/api/client';
+import {GetServerSideProps, InferGetServerSidePropsType} from 'next';
+import {log as _log} from 'next-axiom';
+import {initUrqlClient} from 'next-urql';
+import {Header} from '../components/header';
 import PublicationFooter from '../components/publication-footer';
 
 type Props = {
@@ -23,32 +23,48 @@ type Props = {
     currentMenuId: string;
 }
 
-const Newsletter = (props: Props) => {
-    const { recent3Posts, publication, currentMenuId } = props;
+const About = (props: Props) => {
+    const {recent3Posts, publication, currentMenuId} = props;
 
     const profile = publication.author;
 
     const recentPosts = recent3Posts.map((post: any) => (
-        <BlogPostPreview key={post.id} post={post} publication={publication} />
+        <BlogPostPreview key={post.id} post={post} publication={publication}/>
     ));
     const originalImageSrc =
         publication.favicon ||
         profile?.profilePicture ||
         'https://cdn.hashnode.com/res/hashnode/image/upload/v1600792675173/rY-APy9Fc.png';
 
-    const publicationImageUrl = resizeImage(originalImageSrc, { w: 400, h: 400, c: 'face' });
+    const publicationImageUrl = resizeImage(originalImageSrc, {w: 400, h: 400, c: 'face'});
 
     return (
         <AppProvider publication={publication}>
             <Header currentMenuId={currentMenuId} isHome={false}/>
             <div className="blog-page-area mx-auto min-h-screen px-4 pb-8 pt-20 md:px-10 md:pt-20">
-                <div className="blog-page-card container relative z-30 mx-auto grid grid-flow-row grid-cols-8 pb-0 2xl:grid-cols-10">
+                <div
+                    className="blog-page-card container relative z-30 mx-auto grid grid-flow-row grid-cols-8 pb-0 2xl:grid-cols-10">
                     <div className="col-span-full">
                         <div className="flex">
                             <div>
                                 <h3>Anshul Sanghi</h3>
                             </div>
                         </div>
+                    </div>
+                    <div className="col-span-full">
+            <span className="mx-auto -mb-10 block h-32 w-32 overflow-hidden rounded-full">
+              <CustomImage
+                  originalSrc={originalImageSrc}
+                  src={publicationImageUrl}
+                  alt={publication.title || profile?.name}
+                  className="block w-full"
+                  width={400}
+                  height={400}
+                  priority
+                  layout="responsive"
+              />
+            </span>
+                        <PublicationSubscribeStandOut/>
                     </div>
                 </div>
                 {recent3Posts && recent3Posts.length > 0 && (
@@ -58,7 +74,8 @@ const Newsletter = (props: Props) => {
                                 Recent articles
                             </h3>
                         </div>
-                        <div className="blog-articles-container container mx-auto grid grid-cols-1 gap-10 px-4 md:grid-cols-2 lg:grid-cols-3 xl:px-10 xl:py-10 2xl:px-24 2xl:py-5">
+                        <div
+                            className="blog-articles-container container mx-auto grid grid-cols-1 gap-10 px-4 md:grid-cols-2 lg:grid-cols-3 xl:px-10 xl:py-10 2xl:px-24 2xl:py-5">
                             {recentPosts}
                         </div>
                     </>
@@ -83,9 +100,9 @@ export const getServerSideProps: GetServerSideProps<{
     publication: PublicationFragment;
     recent3Posts: PostThumbnailFragment[];
 }> = async (ctx) => {
-    const { req, res, query } = ctx;
+    const {req, res, query} = ctx;
     const host = process.env.NEXT_PUBLIC_HASHNODE_PUBLICATION_HOST;
-    const log = _log.with({ host });
+    const log = _log.with({host});
 
     const ssrCache = createSSRExchange();
     const urqlClient = initUrqlClient(getUrqlClientConfig(ssrCache), false);
@@ -120,7 +137,7 @@ export const getServerSideProps: GetServerSideProps<{
         };
     }
 
-    const { publication } = publicationInfo.data;
+    const {publication} = publicationInfo.data;
 
     if (!publication.preferences.enabledPages?.newsletter) {
         return {
@@ -145,9 +162,9 @@ export const getServerSideProps: GetServerSideProps<{
         props: {
             publication,
             recent3Posts: publication.recentPosts.edges.map((edge) => edge.node),
-            currentMenuId: 'newsletter'
+            currentMenuId: 'about'
         },
     };
 };
 
-export default Newsletter;
+export default About;
