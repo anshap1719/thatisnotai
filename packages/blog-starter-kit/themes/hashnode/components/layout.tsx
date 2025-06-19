cd64630b6c5eec6c143495d0df7483c95def1857
Anshul Sanghi <anshul@anshulsanghi.tech>
2025-06-19 21:35:46 +0530

    Fix BMC Text

--- packages/blog-starter-kit/themes/hashnode/components/layout.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/layout.tsx
@@ -19,7 +19,7 @@ export const Layout = ({children}: Props) => {
             <Integrations/>
             <script defer data-name="BMC-Widget" data-cfasync="false"
                     src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="anshulandnishank"
-                    data-description="Support me on Buy me a coffee!"
+                    data-description="Support us on Buy me a coffee!"
                     data-message="Thank you for reading! Buy us a coffee if you'd like to support our work further."
                     data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
         </>
