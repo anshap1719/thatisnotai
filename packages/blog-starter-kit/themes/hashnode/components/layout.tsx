f1d103ae99a8eed8465e0f0af88d927e78c19b4a
Anshul Sanghi <anshul@anshulsanghi.tech>
2025-06-19 21:02:37 +0530

    Add Buy Me A Coffee Widget

--- packages/blog-starter-kit/themes/hashnode/components/layout.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/layout.tsx
@@ -1,22 +1,27 @@
-import { Analytics } from './analytics';
-import { Integrations } from './integrations';
-import { Meta } from './meta';
-import { Scripts } from './scripts';
+import {Analytics} from './analytics';
+import {Integrations} from './integrations';
+import {Meta} from './meta';
+import {Scripts} from './scripts';
 
 type Props = {
-	children: React.ReactNode;
+    children: React.ReactNode;
 };
 
-export const Layout = ({ children }: Props) => {
-	return (
-		<>
-			<Meta />
-			<Scripts />
-			<div className="min-h-screen bg-white dark:bg-neutral-950">
-				<main>{children}</main>
-			</div>
-			<Analytics />
-			<Integrations />
-		</>
-	);
+export const Layout = ({children}: Props) => {
+    return (
+        <>
+            <Meta/>
+            <Scripts/>
+            <div className="min-h-screen bg-white dark:bg-neutral-950">
+                <main>{children}</main>
+            </div>
+            <Analytics/>
+            <Integrations/>
+            <script data-name="BMC-Widget" data-cfasync="false"
+                    src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="anshulandnishank"
+                    data-description="Support me on Buy me a coffee!"
+                    data-message="Thank you for reading! Buy us a coffee if you'd like to support our work further."
+                    data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+        </>
+    );
 };
